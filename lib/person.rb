c3204e207838207a8b4ce14a41778dab658f8c2e
bellaappel <bellaappel0@gmail.com>
2020-10-17 14:29:45 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,8 @@ class Person
     attributes.each do |key, value|
       self.class.attr_accessor(key)
       self.send(("#{key}="), value)
+
+      # creating a class called Person. Within that class we are defining a method called initialize, initialize takes in a hash of attributes as an argument. We are iterating over each attribute looking at their key value pair. We are creating an attr_accessor which defines an instance variable for each key. Send establishes the value of the instance variable within this/ of this instance. 
     
     end
   end
